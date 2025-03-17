f0d46586db28fbb4081be9adb6e0eced41bc64e0
monaam <monaam.boussoufi97@gmail.com>
2025-03-17 11:32:59 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/oceane/surveys/SurveysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oceane.surveys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveysBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oceane/surveys/SurveysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceane.surveys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
