147ead82d421fa1a070037b4ec9c7165d315e15a
Fabrice TIERCELIN <fabrice.tiercelin@gmail.com>
2025-03-21 13:38:46 +0100

    Review

--- src/main/java/com/oceane/surveys/entities/Recipient.java
+++ src/main/java/com/oceane/surveys/entities/Recipient.java
@@ -3,19 +3,25 @@ package com.oceane.surveys.entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  */
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
-@Table(name = "RECIPIENT")
+@Table(name = "recipients")
 public class Recipient {
     /**
      *
      */
     @Id
+    @Column(name = "recipient_id")
     @GeneratedValue(
             strategy = GenerationType.IDENTITY
     )
@@ -24,31 +30,41 @@ public class Recipient {
     /**
      * Email du destinataire
      */
-    @Column(name = "EMAIL")
+    @Column(name = "email", nullable = false, unique = true)
     public String email;
 
     /**
      * Prénom
      */
-    @Column(name = "FIRSTNAME")
+    @Column(name = "firstname")
     public String firstname;
 
     /**
      * Nom
      */
-    @Column(name = "LASTNAME")
+    @Column(name = "lastname")
     public String lastname;
 
     /**
      * Entreprise ou client
      */
-    @Column(name = "COMPANY")
+    @Column(name = "company")
     public String company;
 
     /**
      * Type
      */
-    @Column(name = "TYPE")
+    @Column(name = "type", nullable = false)
     @Enumerated(EnumType.STRING)
     public RecipientType type;
+
+    /**
+     * Enquête associée à la personne
+     */
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "survey_recipients",
+            joinColumns = @JoinColumn(name = "recipient_id"),
+            inverseJoinColumns = @JoinColumn(name = "survey_id"))
+    private Set<Survey> surveys = new HashSet<>();
 }
--- src/main/java/com/oceane/surveys/entities/Survey.java
+++ src/main/java/com/oceane/surveys/entities/Survey.java
@@ -1,23 +1,30 @@
 package com.oceane.surveys.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
  */
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
-@Table(name = "SURVEY")
+@Table(name = "surveys")
 public class Survey {
     /**
      *
      */
     @Id
+    @Column(name = "survey_id")
     @GeneratedValue(
             strategy = GenerationType.IDENTITY
     )
@@ -26,37 +33,48 @@ public class Survey {
     /**
      * Titre de l'enquête
      */
-    @Column(name = "TITLE")
+    @Column(name = "title", nullable = false)
     public String title;
 
     /**
      * Description détaillée
      */
-    @Column(name = "DESCRIPTION")
+    @Column(name = "description", length = 1000)
     public String description;
 
     /**
      * Date de création
      */
-    @Column(name = "CREATIONDATE")
+    @Column(name = "creation_date", nullable = false)
     public LocalDateTime creationDate;
 
     /**
      * Date de dernière modification
      */
-    @Column(name = "LASTMODIFIEDDATE")
+    @Column(name = "last_modified_date")
     public LocalDateTime lastModifiedDate;
 
     /**
      * Statut de l'enquête
      */
-    @Column(name = "STATUS")
+    @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
     public SurveyStatus status;
 
     /**
      * Questions associées
      */
-    @OneToMany(mappedBy="surveyId")
-    private List<Question> questions;
+    @OneToMany(mappedBy = "surveys", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Survey> questions = new ArrayList<>();
+
+    /**
+     * Personnes
+     */
+    @ManyToMany
+    @JoinTable(
+            name = "survey_recipients",
+            joinColumns = @JoinColumn(name = "survey_id"),
+            inverseJoinColumns = @JoinColumn(name = "recipient_id")
+    )
+    private Set<Recipient> recipients = new HashSet<>();
 }
