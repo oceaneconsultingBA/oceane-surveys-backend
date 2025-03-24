cd15b3267b424e73506e52f4fb357ae78ab86a2d
Fabrice-TIERCELIN <fabrice.tiercelin@gmail.com>
2025-03-24 12:03:56 +0100

    Review

--- src/main/java/com/oceane/surveys/repositories/QuestionOptionRepository.java
+++ src/main/java/com/oceane/surveys/repositories/QuestionOptionRepository.java
@@ -1,10 +1,15 @@
 package com.oceane.surveys.repositories;
 
+import com.oceane.surveys.entities.Question;
 import com.oceane.surveys.entities.QuestionOption;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface QuestionOptionRepository extends CrudRepository<QuestionOption, Long> {
-    List<QuestionOption> findByQuestionId(Long questionOptionId);
+@Repository
+public interface QuestionOptionRepository extends JpaRepository<QuestionOption, Long> {
+    List<QuestionOption> findByQuestionId(Long questionId);
+    void deleteByQuestionId(Long questionId);
+    List<Question> findByQuestionIdOrderByDisplayOrderAsc(Long questionId);
 }
--- src/main/java/com/oceane/surveys/repositories/QuestionRepository.java
+++ src/main/java/com/oceane/surveys/repositories/QuestionRepository.java
@@ -2,12 +2,17 @@ package com.oceane.surveys.repositories;
 
 import com.oceane.surveys.entities.Question;
 import com.oceane.surveys.entities.QuestionType;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface QuestionRepository extends CrudRepository<Question, Long> {
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findBySurveyId(Long surveyId);
     List<Question> findBySurveyIdAndType(Long surveyId, QuestionType type);
     List<Question> findBySurveyIdAndRequiredTrue(Long surveyId);
+    void deleteBySurveyId(Long surveyId);
+    long countBySurveyId(Long surveyId);
+    List<Question> findBySurveyIdOrderByDisplayOrderAsc(Long surveyId);
 }
